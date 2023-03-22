9f366777be2b8355e1933d1474ccce3217fc9352
Ben Cumming <bcumming@cscs.ch>
2023-03-22 18:22:27 +0100

    wip on v3

--- configure.sh
+++ configure.sh
@@ -1,9 +1,33 @@
 #! /usr/bin/bash
 
+cluster=$CLUSTER_NAME
+echo "=== targeting $cluster"
+
+config_path=$(pwd)/config/$cluster
+if [ ! -d "$config_path" ]
+then
+    echo "ERROR: no config for cluster with name $cluster"
+    exit
+fi
+
+# TODO update this for the target cluster
+store_root="/mch-environment/devt"
+
+echo "=== creating repos.yaml in $config_path/repos.yaml"
+echo "repos:
+- ${store_root}/repo" > $config_path/repos.yaml
+
 recipe_path="$(pwd)/recipe"
 cp ${recipe_path}/Make.user .
 
 mkdir -p tmp store
+
+if [ ! -d "$(pwd)/spack/.git" ]
+then
+    echo "=== installing spack"
+    git clone git@github.com:spack/spack.git
+    (cd spack && git checkout a8b1314d188149e696eb8e7ba3e4d0de548f1894)
+fi
 
 for target in gcc tools nvhpc
 do
@@ -27,5 +51,17 @@ fi
 
 echo "=== patching cray-mpich-binary spack package"
 
-mkdir -p spack/var/spack/repos/builtin/packages/cray-mpich-binary
-cp cray-mpich-binary-package.py spack/var/spack/repos/builtin/packages/cray-mpich-binary/package.py
+if [ ! -d "$(pwd)/store/repo" ]
+then
+    echo "=== copying repo to store"
+    cp -R ${recipe_path}/repo store
+fi
+
+for f in `ls module-config/*.*` Make.user
+do
+    echo UPDATING $f
+    sed -i "s|MOUNTPOINT|${store_root}|g" $f
+done
+
+#mkdir -p spack/var/spack/repos/builtin/packages/cray-mpich-binary
+#cp cray-mpich-binary-package.py spack/var/spack/repos/builtin/packages/cray-mpich-binary/package.py
--- recipe/repo/packages/cray-mpich/package.py
+++ recipe/repo/packages/cray-mpich/package.py
@@ -9,26 +9,46 @@ import spack.compilers
 from spack.package import *
 
 
-class CrayMpichBinary(Package):
+class CrayMpich(Package):
     """Install cray-mpich as a binary package"""
+
+    """Intended to override the main cray-mpich"""
 
     homepage = "https://www.hpe.com/us/en/compute/hpc/hpc-software.html"
     url = "https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.18.4-gcc.tar.gz"
 
     maintainers = ["haampie"]
 
-    version("8.1.21.1-gcc",
-            sha256="0a6852ebf06afd249285fd09566e8489300cba96ad66e90c40df36b6af9a631e",
-            url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.21.1-gcc.tar.gz")
-    version("8.1.21.1-nvhpc",
-            sha256="791b39f2ecb933060abaa8c8704e71da01c6962c4211cc99d12b9d964e9be4cb",
-            url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.21.1-nvhpc.tar.gz")
-    version("8.1.18.4-gcc",
-            sha256="776c695aeed62b3f64a1bca11b30a2537a907777a8664d2f092e3deac288e4ad",
-            url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.18.4-gcc.tar.gz")
-    version("8.1.18.4-nvhpc",
-            sha256="2285433363c75a04ccdf4798be5b0e296e0c9a8fb8fcb38eb0aa4ccf8d1e0843",
-            url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.18.4-nvhpc.tar.gz")
+    version(
+        "8.1.23-gcc",
+        sha256="2d1dfda811848d278548b0d7735f17341c70380dbf7f91dc680e5afcfb5e0038",
+        url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.23-gcc.tar.gz",
+    )
+    version(
+        "8.1.23-nvhpc",
+        sha256="1dd9b161c538dbac564ecff6f1552220ba40dcc9436dc855087438f29861eba1",
+        url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.23-nvhpc.tar.gz",
+    )
+    version(
+        "8.1.21.1-gcc",
+        sha256="0a6852ebf06afd249285fd09566e8489300cba96ad66e90c40df36b6af9a631e",
+        url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.21.1-gcc.tar.gz",
+    )
+    version(
+        "8.1.21.1-nvhpc",
+        sha256="791b39f2ecb933060abaa8c8704e71da01c6962c4211cc99d12b9d964e9be4cb",
+        url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.21.1-nvhpc.tar.gz",
+    )
+    version(
+        "8.1.18.4-gcc",
+        sha256="776c695aeed62b3f64a1bca11b30a2537a907777a8664d2f092e3deac288e4ad",
+        url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.18.4-gcc.tar.gz",
+    )
+    version(
+        "8.1.18.4-nvhpc",
+        sha256="2285433363c75a04ccdf4798be5b0e296e0c9a8fb8fcb38eb0aa4ccf8d1e0843",
+        url="https://jfrog.svc.cscs.ch/artifactory/cray-mpich/cray-mpich-8.1.18.4-nvhpc.tar.gz",
+    )
 
     variant("cuda", default=False)
     variant("rocm", default=False)
@@ -51,6 +71,21 @@ class CrayMpichBinary(Package):
 
     # libfabric.so.1
     depends_on("libfabric@1:", type="link")
+
+    with when("@8.1.23-gcc"):
+        # libgfortran.so.5
+        conflicts("%gcc@:7")
+        for __compiler in spack.compilers.supported_compilers():
+            if __compiler != "gcc":
+                conflicts("%{}".format(__compiler), msg="gcc required")
+
+    with when("@8.1.23-nvhpc"):
+        conflicts("%nvhpc@:20.6")
+        conflicts("+rocm")
+        conflicts("~cuda")
+        for __compiler in spack.compilers.supported_compilers():
+            if __compiler != "nvhpc":
+                conflicts("%{}".format(__compiler), msg="nvhpc required")
 
     with when("@8.1.21.1-gcc"):
         # libgfortran.so.5
@@ -132,6 +167,10 @@ class CrayMpichBinary(Package):
                 if not self.should_patch(f):
                     continue
                 patchelf("--force-rpath", "--set-rpath", rpath, f, fail_on_error=False)
+                # The C compiler wrapper can fail because libmpi_gtl_cuda refers to the symbol
+                # __gxx_personality_v0 but wasn't linked against libstdc++.
+                if "libmpi_gtl_cuda.so" in str(f):
+                    patchelf("--add-needed", "libstdc++.so", f, fail_on_error=False)
 
     @run_after("install")
     def fixup_compiler_paths(self):
@@ -153,4 +192,6 @@ class CrayMpichBinary(Package):
 
         filter_file("@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpicc, string=True)
         filter_file("@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpicxx, string=True)
-        filter_file("@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpifort, string=True)
+        filter_file(
+            "@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpifort, string=True
+        )
