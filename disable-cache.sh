7cbaed140301616c64f4b648678821ec94b3c86d
Ben Cumming <bcumming@cscs.ch>
2023-04-26 18:37:55 +0200

    all the versions of netdcd/hdf5 serial and parallel

--- disable-cache.sh
+++ disable-cache.sh
@@ -3,4 +3,5 @@
 rm -rf Makefile;
 ln -s Makefile.nocache Makefile
 (cd compilers; rm -rf Makefile; ln -s Makefile.nocache Makefile)
+(cd packages; rm -rf Makefile; ln -s Makefile.nocache Makefile)
 
--- enable-cache.sh
+++ enable-cache.sh
@@ -3,4 +3,5 @@
 rm -rf Makefile;
 ln -s Makefile.cache Makefile
 (cd compilers; rm -rf Makefile; ln -s Makefile.cache Makefile)
+(cd packages; rm -rf Makefile; ln -s Makefile.cache Makefile)
 
